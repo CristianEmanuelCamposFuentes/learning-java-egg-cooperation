91417889a2ee33b8bd7749cc3a8280fd726a49ca
Cristian Emanuel Campos Fuentes <cristianemanuelcamposfuentes@hotmail.com>
2023-05-23 19:38:54 -0300

    Agregado primer programa

---
+++ PrimerPrograma/src/primerprograma/HolaMundo.java
@@ -0,0 +1,19 @@
+/*
+    Este programa escribe el texto Hola Mundo!
+ */
+package primerprograma;
+
+/**
+ *
+ * @author crist
+ */
+public class HolaMundo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hola MUNDO !!!");
+    }
+    
+}
