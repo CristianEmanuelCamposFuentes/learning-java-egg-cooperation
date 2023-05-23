d88c1c8727745320ad0b270dd4c44ce04ff98a3a
Cristian Emanuel Campos Fuentes <cristianemanuelcamposfuentes@hotmail.com>
2023-05-23 20:57:46 -0300

    Inicio Ejercicio Intro 2

---
+++ JavaIntroEj02/src/javaintroej02/JavaIntroEj02.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaintroej02;
+
+/**
+ *
+ * @author crist
+ */
+public class JavaIntroEj02 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String nombreAlumno = "Cristian Emanuel";
+        int edadAlumno = 30;
+        double altura = 1.74;
+        boolean estaAprendiendo = true;
+        char inicialFavorita = 'T';
+        short sueldoActual = 3000;
+        
+    }
+    
+}
