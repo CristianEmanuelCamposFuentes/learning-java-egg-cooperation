02b507dfb30d670d7f4bfbeacdcd568403ff5935
Cristian Emanuel Campos Fuentes <cristianemanuelcamposfuentes@hotmail.com>
2023-05-23 20:18:13 -0300

    Inicio Ejercicio Intro 1

---
+++ JavaIntroEj01/src/javaintroej01/JavaIntroEj01.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaintroej01;
+
+/**
+ * EJERCICIO 1
+Crear un proyecto de Java y definir al menos 6 variables en tu IDE de distintos tipos de datos.
+ * @author crist
+ */
+public class JavaIntroEj01 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String nombreAlumno;
+        int edadAlumno;
+        double altura;
+        boolean estaAprendiendo;
+        char inicialFavorita;
+        short sueldoActual;
+        
+        
+    }
+    
+}
