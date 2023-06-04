18ddcbcde497666833749b9ed3026fa553c9ca47
Cristian Emanuel Campos Fuentes <cristianemanuelcamposfuentes@hotmail.com>
2023-06-03 21:33:01 -0300

    Inicio Ejercicio Aprendizaje Extra 1

---
+++ JavaEjAprendExtra01/src/javaejaprendextra01/JavaEjAprendExtra01.java
@@ -0,0 +1,20 @@
+/*
+1. Dado un tiempo en minutos, calcular su equivalente en días y horas. Por ejemplo, si el
+usuario ingresa 1600 minutos, el sistema debe calcular su equivalente: 1 día, 2 horas.
+ */
+package javaejaprendextra01;
+
+/**
+ *
+ * @author crist
+ */
+public class JavaEjAprendExtra01 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
