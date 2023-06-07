d49ffc25929ab3e590359724d0c21ebe134c3787
Cristian Emanuel Campos Fuentes <cristianemanuelcamposfuentes@hotmail.com>
2023-06-07 19:22:46 -0300

    Inicio Ejercicio Aprendizaje Extra 17

---
+++ JavaEjAprendExtra17/src/javaejaprendextra17/JavaEjAprendExtra17.java
@@ -0,0 +1,53 @@
+/*
+17. Crea una aplicación que nos pida un número por teclado y con una función se lo pasamos
+por parámetro para que nos indique si es o no un número primo, debe devolver true si es
+primo, sino false.
+Un número primo es aquel solo puede dividirse entre 1 y si mismo. Por ejemplo: 25 no es
+primo, ya que 25 es divisible entre 5, sin embargo, 17 si es primo.
+¿Qué son los números primos?
+Básicamente, un número primo es un número natural que tiene solo dos divisores o
+factores: 1 y el mismo número. Es decir, es primo aquel número que se puede dividir
+por uno y por el mismo número.
+El primer número primo es 2, y hay 25 números primos entre 1 y 100, ellos son: 2, 3, 5,
+7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89 y 97.
+ */
+package javaejaprendextra17;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author crist
+ */
+public class JavaEjAprendExtra17 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // Lector
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Ingrese un número: ");
+        int numero = scanner.nextInt();
+
+        if (esPrimo(numero)) {
+            System.out.println(numero + " es un número primo.");
+        } else {
+            System.out.println(numero + " no es un número primo.");
+        }
+    }
+
+    public static boolean esPrimo(int numero) {
+        if (numero <= 1) {
+            return false;
+        }
+
+        for (int i = 2; i <= Math.sqrt(numero); i++) {
+            if (numero % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
